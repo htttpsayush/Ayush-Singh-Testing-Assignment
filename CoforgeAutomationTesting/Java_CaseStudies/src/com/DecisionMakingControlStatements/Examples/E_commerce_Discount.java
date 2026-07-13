d5c393cd833496f9ddfdf27df0b3134612cfa48a
AYUSH Jr. <ac.ayush2007@gmail.com>
2026-07-13 23:25:51 +0530

    Azure CaseStudies.

--- CoforgeAutomationTesting/Java_CaseStudies/src/com/DecisionMakingControlStatements/Examples/E_commerce_Discount.java
+++ CoforgeAutomationTesting/Java_CaseStudies/src/com/DecisionMakingControlStatements/Examples/E_commerce_Discount.java
@@ -3,7 +3,14 @@ package com.DecisionMakingControlStatements.Examples;
 public class E_commerce_Discount {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int amount = 6000;
+		
+		if (amount >= 5000) {
+			System.out.println("Discount Available");
+		} else {
+			System.out.println("No Discount");
+		}
 
 	}
 
