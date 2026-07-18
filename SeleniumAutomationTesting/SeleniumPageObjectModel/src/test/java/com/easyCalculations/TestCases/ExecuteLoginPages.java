624fcb86088ab03dc2bc8c6d39071fc1eee180a4
AYUSH Jr. <ac.ayush2007@gmail.com>
2026-07-18 14:21:42 +0530

    Selenium Assingments.

--- SeleniumAutomationTesting/SeleniumPageObjectModel/src/test/java/com/easyCalculations/TestCases/ExecuteLoginPages.java
+++ SeleniumAutomationTesting/SeleniumPageObjectModel/src/test/java/com/easyCalculations/TestCases/ExecuteLoginPages.java
@@ -1,5 +1,27 @@
 package com.easyCalculations.TestCases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.EasyCalculation.Pages.LoginEasyCal;
+
+import Helper.BrowserFactory;
+
 public class ExecuteLoginPages {
 
+    @Test
+    public void CheckValidUser() {
+
+        WebDriver driver = BrowserFactory.BrowserOptions(
+                "chrome",
+                "https://www.login.hiox.com/login?referrer=easycalculation.com");
+
+        LoginEasyCal loginPageEasyCal =
+                PageFactory.initElements(driver, LoginEasyCal.class);
+
+        loginPageEasyCal.login_EasyCal(
+                "9740673180",
+                "raghubn@123");
+    }
 }
